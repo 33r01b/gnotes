1fec5f6eb98a8c3edf0b1581bf9879d458f7bb1c
33r01b <33r01b@gmail.com>
2021-03-04 21:45:50 +0300

    feat: add simple binary search algorithm

---
+++ algorithms/binarysearch.go
@@ -0,0 +1,24 @@
+package algorithms
+
+// BinarySearch return key of list by value
+func BinarySearch(list []int, item int) (mid int, ok bool) {
+	low := 0
+	high := len(list) - 1
+
+	for low <= high {
+		mid = (low + high) / 2
+		guess := list[mid]
+
+		switch {
+		case guess == item:
+			return mid, true
+		case guess > item:
+			high = mid - 1
+			break
+		default:
+			low = mid + 1
+		}
+	}
+
+	return 0, false
+}
---
+++ algorithms/binarysearch_test.go
@@ -0,0 +1,25 @@
+package algorithms
+
+import (
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+	list := []int{1, 12, 13, 41, 51, 63, 117, 800}
+	wantKey := 6
+
+	result, _ := BinarySearch(list, 117)
+
+	if wantKey != result {
+		t.Fatalf("Wrong result, result must be %d, got %d", wantKey, result)
+	}
+}
+
+func TestBinarySearchNotFound(t *testing.T) {
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	result, ok := BinarySearch(list, 10)
+
+	if ok {
+		t.Fatalf("Wrong result, result must be not found, got %d", result)
+	}
+}
