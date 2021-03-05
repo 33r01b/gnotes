e2988b17f80ac5c833a12f77d17fd8837e4cd2b8
33r01b <33r01b@gmail.com>
2021-03-06 01:42:43 +0300

    chore: add issues

--- algorithms/issue/divideandrule.go
+++ algorithms/issue/divideandrule.go
@@ -1,11 +1,73 @@
 package issue
 
+// just issues from book, chapter "divide and rule"
+
 // Sum recursively calculates the sum of list
-// just issue from book, chapter "divide and rule"
 func Sum(list []int) int {
+	switch len(list) {
+	case 0:
+		return 0
+	case 1:
+		return list[0]
+	}
+
+	return list[0] + Sum(list[1:])
+}
+
+// Count recursively calculates the count of list
+func Count(list []int) int {
+	// its stupid, but how else...
 	if len(list) == 0 {
 		return 0
 	}
 
-	return list[0] + Sum(list[1:])
+	return Count(list[1:]) + 1
+}
+
+// Max recursively calculates the max of list
+func Max(list []int) int {
+	switch len(list) {
+	case 0:
+		return 0
+	case 1:
+		return list[0]
+	}
+
+	max := Max(list[1:])
+
+	if list[0] > max {
+		max = list[0]
+	}
+
+	return max
+}
+
+// BinarySearch recursively
+func BinarySearch(list []int, item int) (mid int, ok bool) {
+	switch len(list) {
+	case 0:
+		return 0, false
+	case 1:
+		return 0, list[0] == item
+	}
+
+	high := len(list) - 1
+	mid = high / 2
+	guess := list[mid]
+
+	switch {
+	case guess == item:
+		return mid, true
+	case mid == 1:
+		return 0, false
+	case guess < item:
+		r, ok := BinarySearch(list[mid:], item)
+		if !ok {
+			mid = 0
+		}
+
+		return mid + r, ok
+	}
+
+	return BinarySearch(list[:mid+1], item)
 }
--- algorithms/issue/divideandrule_test.go
+++ algorithms/issue/divideandrule_test.go
@@ -1,6 +1,8 @@
 package issue
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	list := []int{1, 2, 3, 4, 5}
@@ -10,5 +12,82 @@ func TestSum(t *testing.T) {
 
 	if result != want {
 		t.Fatalf("Wrong result %d, want %d", result, want)
+	}
+}
+
+func TestCount(t *testing.T) {
+	list := []int{1, 2, 3, 4, 5}
+	want := 5
+
+	result := Count(list)
+
+	if result != want {
+		t.Fatalf("Wrong result %d, want %d", result, want)
+	}
+}
+
+func TestMax(t *testing.T) {
+	list := []int{1, 2, 3, 4, 5}
+	want := 5
+
+	result := Max(list)
+
+	if result != want {
+		t.Fatalf("Wrong result %d, want %d", result, want)
+	}
+}
+
+func TestBinarySearch(t *testing.T) {
+	list := []int{1, 12, 13, 41, 51, 63, 117, 800}
+
+	testCases := []struct {
+		name    string
+		value   int
+		wantKey int
+	}{
+		{
+			name:    "case 0",
+			value:   1,
+			wantKey: 0,
+		},
+		{
+			name:    "case 1",
+			value:   117,
+			wantKey: 6,
+		},
+		{
+			name:    "case 2",
+			value:   12,
+			wantKey: 1,
+		},
+		{
+			name:    "case 3",
+			value:   13,
+			wantKey: 2,
+		},
+		{
+			name:    "case 4",
+			value:   41,
+			wantKey: 3,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, ok := BinarySearch(list, tc.value)
+
+			if result != tc.wantKey && ok {
+				t.Fatalf("Wrong result %d, want %d", result, tc.wantKey)
+			}
+		})
+	}
+}
+
+func TestBinarySearchNotFound(t *testing.T) {
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	result, ok := BinarySearch(list, 10)
+
+	if ok || result != 0 {
+		t.Fatalf("Wrong result, result must be not found, got %d", result)
 	}
 }
