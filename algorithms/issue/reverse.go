f65b3499ce1bf9e8a988290d1528cfa10560a975
33r01b <33r01b@gmail.com>
2021-03-12 09:57:26 +0300

    fix(issue): reversed slice length

--- algorithms/issue/reverse.go
+++ algorithms/issue/reverse.go
@@ -3,7 +3,7 @@ package issue
 // Reverse text
 // O(n)
 func Reverse(text string) string {
-	reversed := make([]byte, 0, len(text))
+	reversed := make([]byte, len(text))
 
 	j := 0
 	for i := len(text) - 1; i >= 0; i-- {
